ba3a4e52e4fab3e05fe63d3d6b89dcdd5e2edc79
GoShIYO <k024g1025@g.neec.ac.jp>
2026-02-09 12:08:43 +0900

    課題修正

--- 06-01/main.cpp
+++ 06-01/main.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 #include <thread>
+#include <mutex>
+#include <condition_variable>
 
 int main()
 {
-	int num = 0;
+    std::mutex m;
+    std::condition_variable cv;
+    int turn = 1;
 
-	std::thread th1([&]
-		{
-			std::cout << "thread " << num++ << std::endl;
-		});
-	th1.join();
+    auto worker = [&](int id)
+        {
+            std::unique_lock<std::mutex> lk(m);
+            cv.wait(lk, [&] { return turn == id; });
+            std::cout << "thread " << id << std::endl;
+            ++turn;
+            lk.unlock();
+            cv.notify_all();
+        };
 
-	std::thread th2([&]
-		{
-			std::cout << "thread " << num++ << std::endl;
-		});
-	th2.join();
+    std::thread th1(worker, 1);
+    std::thread th2(worker, 2);
+    std::thread th3(worker, 3);
 
-	std::thread th3([&]
-		{
-			std::cout << "thread " << num++ << std::endl;
-		});
-	th3.join();
+    th1.join();
+    th2.join();
+    th3.join();
 
-	return 0;
+    return 0;
 }
