8a4db8cfad1715158f7a7dd5b740963a0836217e
GoShIYO <k024g1025@g.neec.ac.jp>
2026-02-09 12:01:53 +0900

    課題修正

--- 01-01/main.cpp
+++ 01-01/main.cpp
@@ -14,16 +14,18 @@ void PrintStations(list<const char*>& stations, const char* year)
 	printf("\n");
 }
 
-void InsertStation(list<const char*>& stations, const char* targetStation, const char* intsertStation)
+bool InsertStation(list<const char*>& stations, const char* targetStation, const char* insertStation)
 {
 	for (auto it = stations.begin(); it != stations.end(); ++it)
 	{
-		if (strcmp(*it,targetStation) == 0)
+		if (strcmp(*it, targetStation) == 0)
 		{
 			++it;
-			it = stations.insert(it, intsertStation);
+			stations.insert(it, insertStation);
+			return true;
 		}
 	}
+	return false;
 }
 
 int main()
@@ -37,11 +39,17 @@ int main()
 	//1970年
 	PrintStations(stations, "1970");
 	//西日暮里を追加
-	InsertStation(stations, "Nippori", "Nishi-Nippori");
+	if (!InsertStation(stations, "Nippori", "Nishi-Nippori"))
+	{
+		printf("挿入対象の駅 \"%s\" が見つかりませんでした。\n", "Nippori");
+	}
 	//2019年
 	PrintStations(stations, "2019");
 	//高輪ゲードウェイを追加
-	InsertStation(stations, "Shinagawa", "Takanawa Gateway");
+	if (!InsertStation(stations, "Shinagawa", "Takanawa Gateway"))
+	{
+		printf("挿入対象の駅 \"%s\" が見つかりませんでした。\n", "Shinagawa");
+	}
 	//2022年
 	PrintStations(stations, "2022");
 
